d92b0a080e8c3d08bf5c929135d548f97a7c879d
Franki99 <ntaganiradivanni@gmail.com>
2024-12-19 06:45:14 +0200

    added the react app deployment link in my CORS

--- src/main/java/net/enjoy/springboot/ihuzohr/config/SpringSecurity.java
+++ src/main/java/net/enjoy/springboot/ihuzohr/config/SpringSecurity.java
@@ -38,7 +38,7 @@ public class SpringSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174, https://ihuzo-hr-with-react.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Authorization",
